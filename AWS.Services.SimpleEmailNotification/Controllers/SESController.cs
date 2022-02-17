bc2bab7f1a92d665ec3bacc5e723efc8ec453bc0
emmanuel.marinho <emmanuel.marinho@GMEPN005407>
2022-02-17 04:22:53 -0300

    Adicionar arquivos de projeto.

---
+++ AWS.Services.SimpleEmailNotification/Controllers/SESController.cs
@@ -0,0 +1,59 @@
+﻿using Amazon.SimpleEmail;
+using Amazon.SimpleEmail.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AWS.Services.SimpleEmailNotification.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SESController : ControllerBase
+    {
+        private IAmazonSimpleEmailService _amazonSimpleEmailService;
+        private string _fromAddress = "eegito85@gmail.com";
+        private string _toAddress = "eegito85@gmail.com";
+        private string _subject = "SES";
+        private string _body = "<h1>It worked</h1> <p>So happy!!!</p>";
+
+        public SESController(IAmazonSimpleEmailService amazonSimpleEmailService)
+        {
+            _amazonSimpleEmailService = amazonSimpleEmailService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> SendEmail()
+        {
+            SendEmailRequest sendEmailRequest = new SendEmailRequest()
+            {
+                Destination = new Destination() { ToAddresses = new List<string>() { _toAddress } },
+                Message = new Message()
+                {
+                    Body = new Body()
+                    {
+                        Html = new Content()
+                        {
+                            Charset = "UTF-8",
+                            Data = _body
+                        }
+                    },
+                    Subject = new Content()
+                    {
+                        Charset = "UTF-8",
+                        Data = _subject
+                    }
+                },
+                Source = _fromAddress
+            };
+
+            var sendEmailResult = await _amazonSimpleEmailService.SendEmailAsync(sendEmailRequest);
+
+            if(sendEmailResult.HttpStatusCode != System.Net.HttpStatusCode.OK)
+            {
+                return BadRequest("Something went wrong with the request");
+            }
+
+            return Ok("Email was sent succesfully");
+        }
+
+    }
+}
---
+++ AWS.Services.SimpleEmailNotification/Program.cs
@@ -0,0 +1,31 @@
+using Amazon.SimpleEmail;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Configurin AWS services
+builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
+builder.Services.AddAWSService<IAmazonSimpleEmailService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
