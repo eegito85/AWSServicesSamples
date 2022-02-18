8992aa75c1b715452600d1673e2beb82c7634622
Emmanuel Egito Oliveira Candeira Marinho <e_egito@hotmail.com>
2022-02-18 09:14:38 -0300

    Create SQS Function Lamnbda and SQS API

---
+++ AWS.Services.SQS/Controllers/SQSController.cs
@@ -0,0 +1,33 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AWS.Services.SQS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SQSController : ControllerBase
+    {
+        private readonly IAmazonSQS _amazonSQS;
+
+        public SQSController(IAmazonSQS amazonSQS)
+        {
+            _amazonSQS = amazonSQS;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> AddToQueue()
+        {
+            SendMessageRequest request = new SendMessageRequest
+            {
+                QueueUrl = "https://sqs.sa-east-1.amazonaws.com/787152564072/UdemyProjectQueue",
+                MessageBody = $"Test at {DateTime.Now.ToLongTimeString()}"
+            };
+
+            SendMessageResponse result = await _amazonSQS.SendMessageAsync(request);
+
+            return Ok("Hi!");
+        }
+    }
+}
