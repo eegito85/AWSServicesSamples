ec488a51331e6a34262a9405ab769f64a6ebd0f5
Emmanuel Egito Oliveira Candeira Marinho <e_egito@hotmail.com>
2022-02-18 08:25:32 -0300

    Create SNS Api and SNS Lambda Function

---
+++ AWS.Services.SNS/Controllers/SNSController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+
+namespace AWS.Services.SNS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SNSController : ControllerBase
+    {
+        private readonly IAmazonSimpleNotificationService _amazonSimpleNotificationService;
+
+        public SNSController(IAmazonSimpleNotificationService amazonSimpleNotificationService)
+        {
+            _amazonSimpleNotificationService = amazonSimpleNotificationService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> SendMessage()
+        {
+            var request = new PublishRequest
+            {
+                Message = $"Test at {DateTime.Now.ToLongTimeString()}",
+                TopicArn = "arn:aws:sns:sa-east-1:787152564072:UdemyProjectTopic"
+            };
+
+            var response = _amazonSimpleNotificationService.PublishAsync(request);
+
+            return Ok("Message");
+        }
+
+    }
+}
